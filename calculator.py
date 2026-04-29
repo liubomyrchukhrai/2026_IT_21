dcd47c8f02fbab3d0856866b63d3b415d56f2d83
ShuhDemian <shuhdemian@gmail.com>
2026-04-29 11:28:13 +0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -20,22 +20,22 @@ def divide(a, b):
     if b == 0:
         return "Не можна ділити на 0"
     return a / b
-
-print("Оберіть операцію: +, -, *, /,**")
-operation = input("Введіть операцію: ")
-
-num1 = float(input("Введіть перше число: "))
-num2 = float(input("Введіть друге число: "))
-
-if operation == "+":
-    print("Результат:", add(num1, num2))
-elif operation == "-":
-    print("Результат:", subtract(num1, num2))
-elif operation == "*":
-    print("Результат:", multiply(num1, num2))
-elif operation == "/":
-    print("Результат:", divide(num1, num2))
-elif operation == "**":
-    print("Результат:", square(num1,num2))
-else:
-    print("Невідома операція")
+if __name__== "__main__":
+    print("Оберіть операцію: +, -, *, /,**")
+    operation = input("Введіть операцію: ")
+    
+    num1 = float(input("Введіть перше число: "))
+    num2 = float(input("Введіть друге число: "))
+    
+    if operation == "+":
+        print("Результат:", add(num1, num2))
+    elif operation == "-":
+        print("Результат:", subtract(num1, num2))
+    elif operation == "*":
+        print("Результат:", multiply(num1, num2))
+    elif operation == "/":
+        print("Результат:", divide(num1, num2))
+    elif operation == "**":
+        print("Результат:", square(num1,num2))
+    else:
+        print("Невідома операція")
