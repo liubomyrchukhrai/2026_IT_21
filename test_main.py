03556586df6ff50925c85628e0adffae4d2ecb8d
anastasiamalyka <anastasiamalyka@gmail.com>
2026-04-29 11:29:28 +0300

    Implement tests for log function in calculator
    Added tests for logarithm function with various cases.

--- test_main.py
+++ test_main.py
@@ -3,3 +3,22 @@
 def test_example():
     """Simple test"""
     assert 1 + 1 == 2
+
+
+import math
+from calculator.calc import log
+import pytest
+
+def test_log_base10():
+    assert log(100, 10) == 2
+
+def test_log_natural():
+    assert log(math.e, math.e) == 1
+
+def test_log_invalid_value():
+    with pytest.raises(ValueError):
+        log(0)
+
+def test_log_invalid_base():
+    with pytest.raises(ValueError):
+        log(10, 1)
