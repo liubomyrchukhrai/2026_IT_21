d1d7985d15b328852b776a305704e8c077b142f6
ShuhDemian <shuhdemian@gmail.com>
2026-04-29 11:24:51 +0300

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,8 +1,12 @@
-"""Test module"""
+"""Test module calculator"""
+
+import pytest
+from calculator import square
 
 def test_example():
     """Simple test"""
     assert 1 + 1 == 2
+
 def test_square():
     """Test square function"""
     assert square(2,2) == 4
